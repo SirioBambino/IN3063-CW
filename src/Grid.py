9da7fef138e4e028e1f8af16ef089895af93c0ab
Sirio <Sirio.Bambino@city.ac.uk>
2022-12-25 17:06:27 +0000

    Task 1: Implemented dijkstra algorithm
     Implemented dijkstra algorithm to find shortest path and improved class structure, adding a few methods to display paths generated by the pathfinding algorithms

--- src/Grid.py
+++ src/Grid.py
@@ -1,13 +1,14 @@
 import time
 import math
+from queue import PriorityQueue
 import numpy as np
 import matplotlib.pyplot as plt
-from queue import PriorityQueue
+
 
 class Grid:
     def __init__(self, height, width):
         # Set seed for random number generator
-        self.rng = np.random.default_rng(1)
+        self.rng = np.random.default_rng(0)
 
         # Create grid
         self.height = height
@@ -30,14 +31,15 @@ class Grid:
         return grid
 
     # Display the current state of the grid using pyplot
-    def display_grid(self, current_cell):
+    def display_grid(self, current_cells):
         # Create array which will store colour of cells
         cell_colour = np.ones((self.height, self.width, 3))  # Start with an array of white
 
         # Set colour to cells
         cell_colour[self.start_cell] = 0.35, 0.78, 0.28
         cell_colour[self.end_cell] = 0.68, 0.07, 0.07
-        cell_colour[current_cell] = 0.94, 0.79, 0.24
+        for cell in current_cells:
+            cell_colour[cell] = 0.94, 0.79, 0.24
 
         plt.figure(figsize=(self.height * .7, self.width * .7))
         plt.imshow(cell_colour)
@@ -116,13 +118,13 @@ class Grid:
                     current_cell = neighbour
                     break
 
-                # Assign the next cell and the neighbour scores using their distance to the end and their value
-                next_cell_score = math.dist(next_cell, self.end_cell) - (10 - self.grid[next_cell]) / 3
-                neighbour_score = math.dist(neighbour, self.end_cell) - (10 - self.grid[neighbour]) / 3
+                # Assign the next cell and the neighbour costs using their distance to the end and their cost
+                next_cell_cost = math.dist(next_cell, self.end_cell) - (10 - self.grid[next_cell]) / 3
+                neighbour_cost = math.dist(neighbour, self.end_cell) - (10 - self.grid[neighbour]) / 3
 
-                # If the neighbour's score is lower than the next cell score and the neighbour is not the
+                # If the neighbour's cost is lower than the next cell cost and the neighbour is not the
                 # previous cell, make the next cell the neighbour
-                if neighbour_score < next_cell_score and neighbour != previous_cell:
+                if neighbour_cost < next_cell_cost and neighbour != previous_cell:
                     next_cell = neighbour
 
             # If current cell is the end cell, break out of loop
@@ -133,22 +135,87 @@ class Grid:
             previous_cell = current_cell
             current_cell = next_cell
 
-        # Add the current cell to the path and return it
+        # Remove start cell from path, add the end cell and return it
+        path.pop(0)
         path.append(current_cell)
 
         return path
 
+    # Use dijkstra algorithm to return a list of cell indexes that form the shortest path from
+    # the start cell to the end cell
+    def dijkstra(self):
+        path = []
 
-grid = Grid(6, 6)
+        # Add start cell to priority queue with a cost of 0
+        queue = PriorityQueue()
+        queue.put((0, self.start_cell))
 
-path = grid.find_path()
-length = 0
+        costs = {self.start_cell: 0}
+        visited_cells = set()
+        previous_cells = {}
 
-for cell in path:
-    length += grid.grid[cell]
+        # Iterate while queue is not empty
+        while queue.queue:
+            # Pop the lowest cost cell from queue and get its neighbours
+            cost, current_cell = queue.get()
+            neighbours = self.get_cell_neighbours(current_cell)
 
-for cell in path:
-    grid.display_grid(cell)
-    time.sleep(0.25 * grid.grid[cell])
+            # If cell has been visited continue
+            if current_cell in visited_cells:
+                continue
 
-print("Path length =", length)
+            # Add cell to visited cells
+            visited_cells.add(current_cell)
+
+            # Iterate through all the neighbours
+            for neighbour in neighbours:
+                # If neighbour has been visited continue
+                if neighbour in visited_cells:
+                    continue
+
+                # Get the cost to neighbour by adding the current cell's cost and the neighbour's cost
+                cost_to_neighbour = cost + self.grid[neighbour]
+
+                # If the cost to neighbour is less than their current cost, update their cost and add the
+                # neighbour to the queue, also set the neighbour's previous cell to the current cell
+                if cost_to_neighbour < costs.get(neighbour, float('inf')):
+                    queue.put((cost_to_neighbour, neighbour))
+                    previous_cells[neighbour] = current_cell
+                    costs[neighbour] = cost_to_neighbour
+
+        # Create path by starting from the end cell and getting all the previous cells until the start
+        # cell is reached
+        path.append(self.end_cell)
+        previous_cell = previous_cells[self.end_cell]
+        while previous_cell != self.start_cell:
+            path.insert(0, previous_cell)
+            previous_cell = previous_cells[previous_cell]
+
+        return path
+
+    # Use display_grid to display a given path being traversed, waiting an amount of time dependent
+    # on the cell's cost after moving to it
+    def traverse_path(self, path):
+        for cell in path:
+            self.display_grid([cell])
+            time.sleep(0.25 * self.grid[cell])
+
+    # Return the sum of all the cells' costs of a path
+    def get_path_length(self, path):
+        length = 0
+
+        for cell in path:
+            length += self.grid[cell]
+
+        return length
+
+
+grid = Grid(4, 8)
+
+grid.traverse_path(grid.find_path())
+grid.display_grid(grid.find_path())
+print(grid.get_path_length(grid.find_path()))
+
+grid.traverse_path(grid.dijkstra())
+grid.display_grid(grid.dijkstra())
+print(grid.get_path_length(grid.dijkstra()))
