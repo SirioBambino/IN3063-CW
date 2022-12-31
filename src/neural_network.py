7380359bc40b1e482031f3ebd2f2f14f55e2e976
Sirio <Sirio.Bambino@city.ac.uk>
2022-12-31 17:18:40 +0000

    Task 2: Implemented dropout
    Implemented dropout to reduce overfitting when training the model

--- src/neural_network.py
+++ src/neural_network.py
@@ -3,28 +3,29 @@ import numpy as np
 from os.path import dirname, abspath, join
 import idx2numpy
 from matplotlib import pyplot as plt
+from time import perf_counter
 
 
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+def sigmoid(inputs):
+    return 1 / (1 + np.exp(-inputs))
 
-def sigmoid_derivative(x):
-    return x * (1.0 - x)
+def sigmoid_derivative(inputs):
+    return inputs * (1.0 - inputs)
 
-def ReLU(x):
-    return np.maximum(x, 0)
+def ReLU(inputs):
+    return np.maximum(inputs, 0)
 
-def ReLU_derivative(x):
-    return x > 0
+def ReLU_derivative(inputs):
+    return inputs > 0
 
-def softmax(x):
-    return np.exp(x) / sum(np.exp(x))
+def softmax(inputs):
+    return np.exp(inputs) / sum(np.exp(inputs))
 
-def MSE(y_predictions, y_true):
+# MSE: Mean Squared Error
+def calculate_MSE(y_predictions, y_true):
     return np.square(np.subtract(y_true, y_predictions)).mean()
 
-def accuracy(predictions, y):
-    # print(predictions, y)
+def calculate_accuracy(predictions, y):
     return np.sum(predictions == y) / y.size
 
 def one_hot_encoder(x):
@@ -34,9 +35,9 @@ def one_hot_encoder(x):
     return one_hot
 
 
-# https://stackoverflow.com/questions/47493559/valueerror-non-broadcastable-output-operand-with-shape-3-1-doesnt-match-the
 class NeuralNetwork:
 
+    # Initialise the network's parameters, including weights and biases
     def __init__(self, hidden_layers_amount, hidden_nodes_amount, learning_rate, activation_function, seed=None):
         # Set seed for random number generator
         self.rng = np.random.default_rng(seed)
@@ -68,6 +69,7 @@ class NeuralNetwork:
             else:
                 self.biases.append(np.zeros((self.hidden_nodes_amount, 1)))
 
+    # Generate the weights depending on the activation function
     def generate_weights(self, previous_nodes_amount, next_nodes_amount):
         # https://machinelearningmastery.com/weight-initialization-for-deep-learning-neural-networks/
         # Xavier weight initialization
@@ -87,6 +89,7 @@ class NeuralNetwork:
             # scale to the desired range
             return numbers * std
 
+    # Returns the result of the chosen activation function
     def activation_function(self, x):
         activation_functions = ['sigmoid', 'relu']
 
@@ -99,6 +102,7 @@ class NeuralNetwork:
         else:
             return ReLU(x)
 
+    # Returns the result of the derivative of the chosen activation function
     def activation_function_derivative(self, x):
         activation_functions = ['sigmoid', 'relu']
 
@@ -111,48 +115,76 @@ class NeuralNetwork:
         else:
             return ReLU_derivative(x)
 
-    def forward_propagation(self, X):
-        # Perform calculations for each hidden layer + the output layer
-        # Z: Dot product of input and weight + bias, A: Activation of Z
+    # Perform calculations for forward propagation for each layer
+    def forward_propagation(self, X, dropout_rate):
+        # Z: Dot product of input and weight + bias
+        # A: Activation of Z
         Z = []
         A = []
         for i in range(self.hidden_layers_amount + 1):
-            if i == 0:
-                Z.append(np.dot(self.weights[i], X) + self.biases[i])
-                A.append(self.activation_function(Z[i]))
-            # Use softmax function on the output layer
-            elif i == self.hidden_layers_amount:
-                Z.append(np.dot(self.weights[i], A[i-1]) + self.biases[i])
+            # On the output layer find the dot of weights and the previous
+            # layer's A, add the bias and pass the result through the
+            # softmax function
+            if i == self.hidden_layers_amount:
+                Z.append(np.dot(self.weights[i], A[i - 1]) + self.biases[i])
                 A.append(softmax(Z[i]))
-
             else:
-                Z.append(np.dot(self.weights[i], A[i-1]) + self.biases[i])
-                A.append(self.activation_function(Z[i]))
+                # On the input layer find the dot of weights and X, add the
+                # bias and pass the result through the activation function
+                if i == 0:
+                    Z.append(np.dot(self.weights[i], X) + self.biases[i])
+                    A.append(self.activation_function(Z[i]))
+                # On any other layer find the dot of weights and the previous
+                # layer's A, add the bias and pass the result through the
+                # activation function
+                else:
+                    Z.append(np.dot(self.weights[i], A[i-1]) + self.biases[i])
+                    A.append(self.activation_function(Z[i]))
+
+                # Implement dropout by randomly setting some nodes to 0 based on the dropout rate
+                if dropout_rate > 0:
+                    layer = A[len(A)-1].T
+                    input_size, layer_size = layer.shape
+                    # Use dropout rate to calculate the number of nodes to drop out, then create a matrix of random
+                    # indexes that represent the nodes that will be dropped
+                    dropped_nodes_amount = round(layer_size * dropout_rate)
+                    dropped_nodes = rng.integers(0, layer_size, (input_size, layer_size))
+                    dropped_nodes = dropped_nodes.argpartition(dropped_nodes_amount,axis=1)[:,:dropped_nodes_amount]
+                    # Set value of the dropped nodes in the layer to 0
+                    index = 0
+                    for input in dropped_nodes:
+                        for node in input:
+                            layer[index, node] = 0
+                        index += 1
+
         return Z, A
 
-    def backward_propagation(self, Z, A,  X, y):
-        # Perform calculations for each hidden layer + the output layer
+    # Perform calculations for backward propagation for each layer
+    def backward_propagation(self, Z, A, X, y):
         # Z: Dot product of input and weight + bias, A: Activation of Z
         m = X.shape[1]
         Z_derivatives = []
         weights_derivatives = []
         biases_derivatives = []
+        # Loop through the layers backwards starting from the output layer and finishing on the first hidden layer
         for i in range(self.hidden_layers_amount, -1, -1):
+            # Find the derivatives of the weights and biases for each layer
             if i == 0:
                 Z_derivatives.append(np.dot(self.weights[i + 1].T, Z_derivatives[len(Z_derivatives) - 1]) * ReLU_derivative(Z[i]))
-                weights_derivatives.append(1 / m * np.dot(Z_derivatives[i], X.T))
-                biases_derivatives.append(1 / m * np.sum(Z_derivatives[i]))
+                weights_derivatives.append(1 / m * np.dot(Z_derivatives[len(Z_derivatives) - 1], X.T))
+                biases_derivatives.append(1 / m * np.sum(Z_derivatives[len(Z_derivatives) - 1]))
             elif i == self.hidden_layers_amount:
                 Z_derivatives.append(A[i] - one_hot_encoder(y))
                 weights_derivatives.append(1 / m * np.dot(Z_derivatives[0], A[i - 1].T))
                 biases_derivatives.append(1 / m * np.sum(Z_derivatives[0]))
             else:
-                Z_derivatives.append(np.dot(self.weights[i + 1], Z_derivatives[len(Z_derivatives)-1]) * ReLU_derivative(Z[i]))
-                weights_derivatives.append(1 / m * np.dot(Z_derivatives[len(Z_derivatives)-1], A[i - 1].T))
-                biases_derivatives.append(1 / m * np.sum(Z_derivatives[len(Z_derivatives)-1]))
+                Z_derivatives.append(np.dot(self.weights[i + 1].T, Z_derivatives[len(Z_derivatives) - 1]) * ReLU_derivative(Z[i]))
+                weights_derivatives.append(1 / m * np.dot(Z_derivatives[len(Z_derivatives) - 1], A[i - 1].T))
+                biases_derivatives.append(1 / m * np.sum(Z_derivatives[len(Z_derivatives) - 1]))
 
         return weights_derivatives, biases_derivatives
 
+    # Update weights and biases using their derivatives and the learning rate
     def update_parameters(self, weights_derivatives, biases_derivatives):
         weights_derivatives.reverse()
         biases_derivatives.reverse()
@@ -160,47 +192,60 @@ class NeuralNetwork:
             self.weights[i] -= self.learning_rate * weights_derivatives[i]
             self.biases[i] -= self.biases[i] - self.learning_rate * biases_derivatives[i]
 
-    def fit(self, X, y, epochs, batch_size, stopping_patience):
+    # Train the network against the given data
+    def fit(self, X, y, epochs, batch_size, stopping_threshold, dropout_rate):
         losses = []
         for i in range(epochs):
+            # Start counter to calculate run time of epoch
+            start = perf_counter()
+
             # Shuffle the data
             # shuffler = self.rng.permutation(len(X))
             # X = X.T[shuffler]
             # y = y[shuffler]
             #
-            # Z = []
-            # A = []
             # for start in range(0, X.shape[0], batch_size):
             #     end = start + batch_size
             #     X_batch = X[start:end].T
             #     y_batch = y[start:end]
 
-            # Z: Dot product of input and weight + bias, A: Activation of Z
-            # Input vector: X, Output vector: Y
-            Z, A = self.forward_propagation(X)
+            # Perform forward propagation, backward propagation and then update the weights and biases
+            Z, A = self.forward_propagation(X, dropout_rate)
             weights_derivatives, biases_derivatives = self.backward_propagation(Z, A, X, y)
             self.update_parameters(weights_derivatives, biases_derivatives)
 
+            # The network's predictions after an epoch cycle
+            #Z, A = self.forward_propagation(X, 0)
             predictions = self.get_predictions(A[self.hidden_layers_amount])
-            loss = MSE(predictions, y)
+            # Calculate the accuracy and mean squared error using the predictions
+            accuracy = calculate_accuracy(predictions, y)
+            loss = calculate_MSE(predictions, y)
 
+            # End counter to calculate run time of epoch
+            end = perf_counter()
+
+            # Print some information every 10 epochs
             if i % 10 == 0:
-                print("Epoch: ", i)
-                Z, A = self.forward_propagation(X)
-                print("Accuracy: {0:.4f}".format(accuracy(predictions, y)), "Loss: {0:.4f}".format(loss))
+                print("Epoch: {0}/{1}".format(i, epochs), "Computation time: {0:.2f}ms".format((end-start)*1000))
+                # Z, A = self.forward_propagation(X)
+                print("Accuracy: {0:.4f}".format(accuracy), "Loss: {0:.4f}\n".format(loss))
 
-            # Implement early stopping if loss doesn't improve
+            # Implement early stopping if loss doesn't improve fast enough
             losses.append(loss)
-            if len(losses) > stopping_patience and loss - losses[len(losses) - stopping_patience] > -0.1:
-                print("Early stopping")
-                print("Accuracy: {0:.4f}".format(accuracy(predictions, y)), "Loss: {0:.4f}".format(loss))
+            stopping_patience = round(epochs * 0.25)
+            # Stop the loop if in the last 25% of epochs the loss has dropped less than the stopping threshold
+            if len(losses) > stopping_patience and loss - losses[len(losses) - stopping_patience] > -stopping_threshold:
+                print("Early stopping | Epoch: {0}/{1}".format(i, epochs))
+                print("Accuracy: {0:.4f}".format(accuracy), "Loss: {0:.4f}".format(loss))
                 break
+        plt.plot(losses)
+        plt.show()
 
     def get_predictions(self, output_layer):
         return np.argmax(output_layer, 0)
 
     def make_predictions(self, X):
-        _, A = self.forward_propagation(X)
+        _, A = self.forward_propagation(X, 0)
         predictions = self.get_predictions(A[len(A)-1])
         return predictions
 
@@ -245,9 +290,9 @@ training_data_Y = training_data[0]
 training_data_X = training_data[1:column_amount] / 255
 
 # Initialise model and fit it to the training data
-model = NeuralNetwork(hidden_layers_amount=1, hidden_nodes_amount=10, learning_rate=.1, activation_function="relu", seed=seed)
-model.fit(training_data_X, training_data_Y, epochs=500, batch_size=10, stopping_patience=50)
+model = NeuralNetwork(hidden_layers_amount=1, hidden_nodes_amount=20, learning_rate=.2, activation_function="relu", seed=seed)
+model.fit(training_data_X, training_data_Y, epochs=500, batch_size=50, stopping_threshold=-1, dropout_rate=0.5)
 
 # Test the model against the testing data
 test_predictions = model.make_predictions(testing_data_X)
-print("\nAccuracy on testing data:", accuracy(test_predictions, testing_data_Y))
+print("\nAccuracy on testing data:", calculate_accuracy(test_predictions, testing_data_Y))
