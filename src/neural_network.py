bce2aefc447d88d9ad00d53446ed87bc187734ef
Sirio <Sirio.Bambino@city.ac.uk>
2022-12-31 01:47:03 +0000

    Task 2: Implement stopping criterion

--- src/neural_network.py
+++ src/neural_network.py
@@ -5,31 +5,30 @@ import idx2numpy
 from matplotlib import pyplot as plt
 
 
-# The Sigmoid function, which describes an S shaped curve.
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
-
-# The derivative of the Sigmoid function.
-# This is the gradient of the Sigmoid curve.
 def sigmoid_derivative(x):
     return x * (1.0 - x)
-
 
 def ReLU(x):
     return np.maximum(x, 0)
 
-
 def ReLU_derivative(x):
     return x > 0
-
 
 def softmax(x):
     return np.exp(x) / sum(np.exp(x))
 
+def MSE(y_predictions, y_true):
+    return np.square(np.subtract(y_true, y_predictions)).mean()
+
+def accuracy(predictions, y):
+    # print(predictions, y)
+    return np.sum(predictions == y) / y.size
 
 def one_hot_encoder(x):
-    one_hot = np.zeros((x.size, x.max() + 1))
+    one_hot = np.zeros((x.size, 10))
     one_hot[np.arange(x.size), x] = 1
     one_hot = one_hot.T
     return one_hot
@@ -112,14 +111,14 @@ class NeuralNetwork:
         else:
             return ReLU_derivative(x)
 
-    def forward_propagation(self, input_vector):
+    def forward_propagation(self, X):
         # Perform calculations for each hidden layer + the output layer
         # Z: Dot product of input and weight + bias, A: Activation of Z
         Z = []
         A = []
         for i in range(self.hidden_layers_amount + 1):
             if i == 0:
-                Z.append(np.dot(self.weights[i], input_vector) + self.biases[i])
+                Z.append(np.dot(self.weights[i], X) + self.biases[i])
                 A.append(self.activation_function(Z[i]))
             # Use softmax function on the output layer
             elif i == self.hidden_layers_amount:
@@ -131,21 +130,20 @@ class NeuralNetwork:
                 A.append(self.activation_function(Z[i]))
         return Z, A
 
-    def backward_propagation(self, Z, A,  input_vector, output_vector):
+    def backward_propagation(self, Z, A,  X, y):
         # Perform calculations for each hidden layer + the output layer
         # Z: Dot product of input and weight + bias, A: Activation of Z
-        # input_vector.shape[1]: m
-        m = input_vector.shape[1]
+        m = X.shape[1]
         Z_derivatives = []
         weights_derivatives = []
         biases_derivatives = []
         for i in range(self.hidden_layers_amount, -1, -1):
             if i == 0:
                 Z_derivatives.append(np.dot(self.weights[i + 1].T, Z_derivatives[len(Z_derivatives) - 1]) * ReLU_derivative(Z[i]))
-                weights_derivatives.append(1 / m * np.dot(Z_derivatives[i], input_vector.T))
+                weights_derivatives.append(1 / m * np.dot(Z_derivatives[i], X.T))
                 biases_derivatives.append(1 / m * np.sum(Z_derivatives[i]))
             elif i == self.hidden_layers_amount:
-                Z_derivatives.append(A[i] - one_hot_encoder(output_vector))
+                Z_derivatives.append(A[i] - one_hot_encoder(y))
                 weights_derivatives.append(1 / m * np.dot(Z_derivatives[0], A[i - 1].T))
                 biases_derivatives.append(1 / m * np.sum(Z_derivatives[0]))
             else:
@@ -159,37 +157,66 @@ class NeuralNetwork:
         weights_derivatives.reverse()
         biases_derivatives.reverse()
         for i in range(self.hidden_layers_amount + 1):
-            self.weights[i] = self.weights[i] - self.learning_rate * weights_derivatives[i]
-            self.biases[i] = self.biases[i] - self.learning_rate * biases_derivatives[i]
+            self.weights[i] -= self.learning_rate * weights_derivatives[i]
+            self.biases[i] -= self.biases[i] - self.learning_rate * biases_derivatives[i]
 
-    # Need to implement SGD
-    def gradient_descent(self, input_vector, output_vector, epochs):
+    def fit(self, X, y, epochs, batch_size, stopping_patience):
+        losses = []
         for i in range(epochs):
-            Z = []
-            A = []
+            # Shuffle the data
+            # shuffler = self.rng.permutation(len(X))
+            # X = X.T[shuffler]
+            # y = y[shuffler]
+            #
+            # Z = []
+            # A = []
+            # for start in range(0, X.shape[0], batch_size):
+            #     end = start + batch_size
+            #     X_batch = X[start:end].T
+            #     y_batch = y[start:end]
+
             # Z: Dot product of input and weight + bias, A: Activation of Z
             # Input vector: X, Output vector: Y
-            Z, A = self.forward_propagation(input_vector)
-            weights_derivatives, biases_derivatives = self.backward_propagation(Z, A, input_vector, output_vector)
+            Z, A = self.forward_propagation(X)
+            weights_derivatives, biases_derivatives = self.backward_propagation(Z, A, X, y)
             self.update_parameters(weights_derivatives, biases_derivatives)
+
+            predictions = self.get_predictions(A[self.hidden_layers_amount])
+            loss = MSE(predictions, y)
+
             if i % 10 == 0:
-                print("Iteration: ", i)
-                predictions = self.get_predictions(A[self.hidden_layers_amount])
-                print(self.get_accuracy(predictions, output_vector))
+                print("Epoch: ", i)
+                Z, A = self.forward_propagation(X)
+                print("Accuracy: {0:.4f}".format(accuracy(predictions, y)), "Loss: {0:.4f}".format(loss))
+
+            # Implement early stopping if loss doesn't improve
+            losses.append(loss)
+            if len(losses) > stopping_patience and loss - losses[len(losses) - stopping_patience] > -0.1:
+                print("Early stopping")
+                print("Accuracy: {0:.4f}".format(accuracy(predictions, y)), "Loss: {0:.4f}".format(loss))
+                break
 
     def get_predictions(self, output_layer):
         return np.argmax(output_layer, 0)
 
-    def get_accuracy(self, predictions, output_vector):
-        print(predictions, output_vector)
-        return np.sum(predictions == output_vector) / output_vector.size
-
-    def make_predictions(self, input_vector):
-        _, output_layer = self.forward_propagation(input_vector)
-        predictions = self.get_predictions(output_layer)
+    def make_predictions(self, X):
+        _, A = self.forward_propagation(X)
+        predictions = self.get_predictions(A[len(A)-1])
         return predictions
 
+    def test_prediction(self, index, X, y):
+        current_image = X[:, index, None]
+        prediction = self.make_predictions(X[:, index, None])
+        label = y[index]
+        print("Prediction: ", prediction)
+        print("Label: ", label)
 
+        current_image = current_image.reshape((28, 28)) * 255
+        plt.imshow(current_image, cmap=plt.cm.binary)
+        plt.show()
+
+
+# Set the seed
 seed = 1
 rng = np.random.default_rng(seed)
 
@@ -202,11 +229,12 @@ label_file = join(data_path, 'train-labels-idx1-ubyte')
 image_array = idx2numpy.convert_from_file(image_file)
 label_array = idx2numpy.convert_from_file(label_file)
 
-# Reshape the data so it's 60000, 784 instead of 60000, 28, 28 and add a label column
+# Reshape the data so it's 60000, 784 instead of 60000, 28, 28 and add label column
 data = np.c_[label_array, image_array.reshape(60000, 784)]
 
+# Shuffle the data and split it into training and testing sets
 row_amount, column_amount = data.shape
-rng.shuffle(data)  # shuffle before splitting into dev and training sets
+rng.shuffle(data)
 
 testing_data = data[0:15000].T
 testing_data_Y = testing_data[0]
@@ -215,14 +243,11 @@ testing_data_X = testing_data[1:column_amount] / 255
 training_data = data[45000:row_amount].T
 training_data_Y = training_data[0]
 training_data_X = training_data[1:column_amount] / 255
-_, training_data_column_amount = training_data_X.shape
 
+# Initialise model and fit it to the training data
 model = NeuralNetwork(hidden_layers_amount=1, hidden_nodes_amount=10, learning_rate=.1, activation_function="relu", seed=seed)
+model.fit(training_data_X, training_data_Y, epochs=500, batch_size=10, stopping_patience=50)
 
-model.gradient_descent(training_data_X, training_data_Y, epochs=500)
-
-'''
-print(train_label_array[70])
-plt.imshow(train_image_array[70], cmap=plt.cm.binary)
-plt.show()
-'''
+# Test the model against the testing data
+test_predictions = model.make_predictions(testing_data_X)
+print("\nAccuracy on testing data:", accuracy(test_predictions, testing_data_Y))
